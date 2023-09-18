912a2c589b65c2ac65464bc7b8e9a6eefaf605a3
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-18 23:00:52 +0000

    making html cimoonenbt

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+export default class TrafficLight extends React.Component {
+	render(){
+		return <div>
+			<div id="trafficTop"></div>
+			<div id="container"></div>
+			<div className = {"red light "+extraYellowClass}
+				onClick={()=> this.setState({color: "red"})}
+			></div>
+			<div className = {"yellow light "+extraYellowClass}
+				onClick={()=> this.setState({color: "yellow"})}
+			></div>
+			<div className = {"green light "+extraYellowClass}
+				onClick={()=> this.setState({color: "green"})}
+			></div>
+		</div>;
 
-export default Home;
+	}
+}
