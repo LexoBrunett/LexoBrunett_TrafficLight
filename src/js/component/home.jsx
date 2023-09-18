8b0ce303ae212ef6983d74d943dc671b22818eaa
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2023-09-18 23:40:29 +0000

    correcting errors

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,41 @@
 import React, { useState } from "react";
+import "../../styles/index.css"
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
 
 
-export default class TrafficLight extends React.Component {
-	render(){
-		return <div>
-			<div id="trafficTop"></div>
-			<div id="container"></div>
-			<div className = {"red light "+extraYellowClass}
-				onClick={()=> this.setState({color: "red"})}
-			></div>
-			<div className = {"yellow light "+extraYellowClass}
-				onClick={()=> this.setState({color: "yellow"})}
-			></div>
-			<div className = {"green light "+extraYellowClass}
-				onClick={()=> this.setState({color: "green"})}
-			></div>
-		</div>;
+export function trafficLights() {
 
-	}
+	const [selectedColor, setSelectedColor] = useState("red");
+	console.log(selectedColor);
+	return (
+		<div>
+			<div className="traffic-post">
+
+			</div>
+			<div className="traffic-light">
+
+				<div onClick={() => (setSelectedColor("red"))} className={"light red" + (selectedColor === "red" ? " glow" : "")
+				}></div>
+
+
+
+				<div onClick={() => (setSelectedColor("yellow"))} className={"light yellow" + (selectedColor === "yellow" ? " glowyellow" : "")
+				}></div>
+
+
+
+				<div onClick={() => (setSelectedColor("green"))} className={"light green" + (selectedColor === "green" ? " glowgreen" : "")
+				}>
+				</div>
+
+			</div>
+		</div >
+
+	);
 }
+console.log("Hello")
+export default trafficLights;
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,38 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+export function App() {
+
+    const [selectedcolor, setSelectedColor] = useState("red");
+
+    return (
+        <div>
+            <div className="traffic-light">
+
+                <div className={
+                    "light red" + (selectedColor === "red" ? " glow" : "")
+                }></div>
+
+                <div classsName={"light yellow" + (selectedColor === "yellow" ? " glow" : "")
+                }></div>
+
+            </div>
+
+            <div className={"light green" + (selectedColor === "green" ? " glow" : "")
+            }>
+
+            </div>
+
+
+        </div >
+
+    );
+}
+console.log("Hello")
+
+
+
+
+
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
